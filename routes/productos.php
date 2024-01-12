98dc449ae6f8744dfca2579b9aa912d612b6f411
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2024-01-12 10:53:29 -0500

    Se actualizan las rutas de productos.php para que todas las rutas a excepción de login y registre se consuman validando el jwt, login y registre usarán el token básico

--- routes/productos.php
+++ routes/productos.php
@@ -4,11 +4,11 @@ use ApiMegaplex\Controllers\ProductosController;
 use ApiMegaplex\Models\UserEliteNut;
 
 $app->response()->header('Content-Type', 'application/json;charset=UTF-8'); //Para que devuelva un json por defecto
-$app->get('/api/prueba', 'tokenJwt', ProductosController::class . ':prueba');
-$app->get('/api/productos', 'tokenHeader', ProductosController::class . ':getProductos');
 $app->post('/api/login', 'tokenHeader', ProductosController::class . ':login');
 $app->post('/api/registre', 'tokenHeader', ProductosController::class . ':registre');
-$app->post('/api/productos/subir-imagen', 'tokenHeader', ProductosController::class . ':subirImagen');
-$app->post('/api/productos/subir-imagen/file', 'tokenHeader', ProductosController::class . ':subirImagenFile');
-$app->post('/api/productos', 'tokenHeader', ProductosController::class . ':insertarProducto');
+$app->get('/api/prueba', 'tokenJwt', ProductosController::class . ':prueba');
+$app->get('/api/productos', 'tokenJwt', ProductosController::class . ':getProductos');
+$app->post('/api/productos/subir-imagen', 'tokenJwt', ProductosController::class . ':subirImagen');
+$app->post('/api/productos/subir-imagen/file', 'tokenJwt', ProductosController::class . ':subirImagenFile');
+$app->post('/api/productos', 'tokenJwt', ProductosController::class . ':insertarProducto');
 
